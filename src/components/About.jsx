3a081eb3bea493fb843b506d7429e9c9b935fc1e
Valarie Oyieke <oyiekeav@gmail.com>
2023-05-09 16:31:37 +0300

    update components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ const About = () => {
     return (  
         <div className="about">
           <h2>About</h2>
-          <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. 
+          <p>I am a Frontend developer with a particular interest in making things simple and automating daily tasks. 
              I try to keep up with security and best practices, 
              and I'm always looking for new things to learn.</p>
         </div>
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -9,7 +9,7 @@ const Info = () => {
         <div className="info">
             <img src={img} className="img"/>
             <h1 className="title">Valarie Oyieke</h1>
-            <h3 className="subtitle">Front-end Developer</h3>
+            <h3 className="subtitle">Frontend Developer</h3>
             <div className="btns">
                 <button className="email-btn"><MdEmail size={16}/>Email</button>
                 <button onClick={()=>newTab('https://www.linkedin.com/in/valarie-oyieke-92b8a3185')}className="linkedin-btn"><FaLinkedin size={16}/> LinkedIn</button>
